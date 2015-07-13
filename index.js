fd6df866704898998c1f049101dc5f92994b35db
Suraj bk <bksuraj1994@gmail.com>
2015-07-13 16:30:55 +0530

    Config

--- index.js
+++ index.js
@@ -9,28 +9,36 @@ var express = require('express')
     , dbConfig = require('./db')
     , routes = require('./routes')
     , mongoose = require('mongoose')
+    , config = require('./config/main-config')
     , MongoClient = require('mongodb').MongoClient;
 
-// Connect to DB
-mongoose.connect(dbConfig.url);
+
+var CONNECT_URL='';  //CONNECTION URL FOR MONGODB
+var DB_URL='';
+// 'admin:rCcJbBS7zb-g@127.0.0.1:51881/website';
 
 
-//var connection_string = 'localhost:27017/nodeblog';
-var connection_string = 'admin:rCcJbBS7zb-g@127.0.0.1:51881/website';
-// if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){                                //IF RUNNING ON OPENSHIFT
+  DB_URL = 'mongodb://' + config.OPENSHIFT_MONGODB_USERNAME + ':' +
+                               config.OPENSHIFT_MONGODB_PASSWORD  + '@' +
+                               config.OPENSHIFT_MONGODB_HOSTNAME + ':' +
+                               config.OPENSHIFT_MONGODB_PORT + '/';
+  CONNECT_URL = DB_URL + config.OPENSHIFT_DB_NAME;
+}else{                                                                        //IF RUNNING LOCALLY
+  DB_URL = 'mongodb://' + config.LOCAL_MONGODB_USERNAME +
+                               config.LOCAL_MONGODB_PASSWORD  +
+                               config.LOCAL_MONGODB_HOSTNAME + ':' +
+                               config.LOCAL_MONGODB_PORT + '/';
+  CONNECT_URL = DB_URL + config.LOCAL_DB_NAME;
 }
+console.log(CONNECT_URL);
 
-MongoClient.connect('mongodb://'+ connection_string, function(err, db) {
+
+MongoClient.connect(CONNECT_URL, function(err, db) {
+  
 
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'jade');
-
     app.use(favicon());
     app.use(logger('dev'));
     app.use(bodyParser.json());
@@ -38,31 +46,16 @@ MongoClient.connect('mongodb://'+ connection_string, function(err, db) {
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
 
-    var passport = require('passport');
-    var expressSession = require('express-session');
-
-    app.use(expressSession({secret: 'mySecretKey'}));
-    app.use(passport.initialize());
-    app.use(passport.session());
-
-    var flash = require('connect-flash');
-    app.use(flash());
-
-    // Initialize Passport
-    var initPassport = require('./passport/init');
-    initPassport(passport);
-
-    var routes1 = require('./routes/admin')(passport,urlencodedParser);
-    app.use('/admin', routes1);
-
-    var routes2 = require('./routes/writer')(passport,urlencodedParser);
-    app.use('/writer', routes2);
-
+    //INCLUDE THE USER ROUTES
     routes(app,db);
 
-    var ip_addr = process.env.OPENSHIFT_NODEJS_IP   || 'localhost';
-    var port    = process.env.OPENSHIFT_NODEJS_PORT || '5555';
+    //INCLUDE THE ADMIN ROUTES
+    app.use('/admin', require('./routes/admin1')(db,DB_URL)); 
+    app.use('/writer', require('./routes/writer')(db,DB_URL));
 
+    var ip_addr = config.EXPRESS_IP_ADDRESS;
+    var port = config.EXPRESS_PORT_ADDRESS;
+    //START THE APP
     app.listen(port,ip_addr);
-    console.log('Express server listening on : '+ip_addr + ' : ' + port);
+    console.log('Express server listening on http://'+ip_addr + ':' + port);
 });
